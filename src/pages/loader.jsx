1224b4ee13b8360b3af39456fc7701ca6bd28fa8
Marc Smith <marcsmith@MacBook-Pro-2.local>
2024-05-14 08:49:55 -0400

    make mobile friendly

---
+++ src/pages/loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../assets/MESlogo-01.gif';
+
+export const Loader = () => {
+    return (
+        <div>
+            <img src={logo} alt="Loading..." />
+        </div>
+    );
+};
